bd621e870857f94f4327573ec8ef18f8c3aed1bb
manuelfarje <manuel.farje@rwth-aachen.de>
2014-04-16 08:49:52 +0200

    update heating.rb

--- models/heating.rb
+++ models/heating.rb
@@ -1,2 +1,15 @@
 class Heating
+
+# Accessors werden gelistet
+attr_accessor :name, :area, :temperature, :efficiency
+
+# Initialisierung 
+	def initialize(name, area,temperature)  
+		# Instance Variablen   
+		@name = name  
+		@area = area
+		@temperature = temperature
+		@efficiency = area/temperature
+	end  
+
 end
